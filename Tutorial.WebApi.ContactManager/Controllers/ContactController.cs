585e0744b7760b95b2bf7e0e33bfe26a03240619
thebrunnings@gmail.com <thebrunnings@gmail.com>
2014-06-09 18:07:04 +0100

    Dummy

---
+++ Tutorial.WebApi.ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tutorial.WebApi.ContactManager.Models;
+using Tutorial.WebApi.ContactManager.Services;
+
+namespace Tutorial.WebApi.ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        } 
+
+        public Contact[] Get()
+        {
+            return this.contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
---
+++ Tutorial.WebApi.ContactManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tutorial.WebApi.ContactManager.WebApiApplication" Language="C#" %>
